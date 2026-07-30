308ad3a8827c355a145998509e00fc4506fb73a7
NegarMov <movaghatian.negar@gmail.com>
2026-07-30 18:29:15 +0330

    add gsap animation

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { gsap } from 'gsap';
+
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,6 +8,35 @@ import { Component } from '@angular/core';
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements AfterViewInit {
+
+  @ViewChild('container')
+  container!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit() {
+    gsap.to(this.container.nativeElement, {
+    y: -12,
+    duration: 2.7,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+  });
+
+  gsap.to(this.container.nativeElement, {
+    x: 5,
+    duration: 4.1,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+  });
+
+  gsap.to(this.container.nativeElement, {
+    rotation: 1.5,
+    duration: 3.3,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut"
+  });
+  }
 
 }
