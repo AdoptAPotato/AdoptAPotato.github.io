6f1035f161e231c60642bd800d43514018007296
NegarMov <movaghatian.negar@gmail.com>
2026-08-03 18:53:25 +0330

    create the stepper

--- src/app/pages/character-creator/character-creator.component.ts
+++ src/app/pages/character-creator/character-creator.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { CreatorWizardComponent } from '../../components/creator-wizard/creator-wizard.component';
 import { CreatorExplanationComponent } from '../../components/creator-explanation/creator-explanation.component';
 
 @Component({
   selector: 'app-character-creator',
-  imports: [FormsModule, CreatorWizardComponent, CreatorExplanationComponent],
+  imports: [CommonModule, FormsModule, CreatorWizardComponent, CreatorExplanationComponent],
   templateUrl: './character-creator.component.html',
   styleUrl: './character-creator.component.css'
 })
 export class CharacterCreatorComponent {
 
+  currentStep: number = 1;
+  maxSteps: number = 3
+  
   potatoName: string = 'Max the POTATO';
+
+  next() {
+    this.currentStep++;
+  }
+
+  previous() {
+    this.currentStep--;
+  }
 
 }
