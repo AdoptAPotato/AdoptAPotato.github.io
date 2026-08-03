549e537035841c59e62a9f2e4aad1206a338cd37
NegarMov <movaghatian.negar@gmail.com>
2026-08-03 10:44:19 +0330

    setup the home page with basic styling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TextPlugin } from 'gsap/TextPlugin';
+import { gsap } from 'gsap';
+
+gsap.registerPlugin(TextPlugin);
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent  implements AfterViewInit {
+
+  @ViewChild('title') title!: ElementRef<HTMLSpanElement>;
+  @ViewChild('cursor') cursor!: ElementRef<HTMLSpanElement>;
+
+  ngAfterViewInit() {
+    const tl = gsap.timeline();
+
+    tl.to(this.title.nativeElement, {
+      duration: 2,
+      text: { value: 'یه سیب‌زمینی به سرپرستی بگیر!' },
+      ease: 'none'
+    }).to(this.cursor.nativeElement, {
+      autoAlpha: 0
+    });
+  }
 
 }
