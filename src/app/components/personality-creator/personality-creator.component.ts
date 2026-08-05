d4522d116ade94a915bd3c1f4c70b4251140460d
NegarMov <movaghatian.negar@gmail.com>
2026-08-05 21:28:46 +0330

    style selects

--- src/app/components/personality-creator/personality-creator.component.ts
+++ src/app/components/personality-creator/personality-creator.component.ts
@@ -1,37 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { CreatorStateService } from '../../services/creator-state.service';
 
 @Component({
   selector: 'app-personality-creator',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './personality-creator.component.html',
   styleUrl: './personality-creator.component.css'
 })
 export class PersonalityCreatorComponent {
 
-  mbtiTypes: string[] = [
-    'INTJ',
-    'INTP',
-    'ENTJ',
-    'ENTP',
+  mbtiTypes = [
+    { code: 'INTJ', name: 'معمار', color: '#5c4dce' },
+    { code: 'INTP', name: 'متفکر', color: '#5c4dce' },
+    { code: 'ENTJ', name: 'فرمانده', color: '#5c4dce' },
+    { code: 'ENTP', name: 'مجادل', color: '#5c4dce' },
 
-    'INFJ',
-    'INFP',
-    'ENFJ',
-    'ENFP',
+    { code: 'INFJ', name: 'حامی', color: '#3fc756' },
+    { code: 'INFP', name: 'میانجی', color: '#3fc756' },
+    { code: 'ENFJ', name: 'قهرمان', color: '#3fc756' },
+    { code: 'ENFP', name: 'کاوشگر', color: '#3fc756' },
 
-    'ISTJ',
-    'ISFJ',
-    'ESTJ',
-    'ESFJ',
+    { code: 'ISTJ', name: 'بازرس', color: '#5fafec' },
+    { code: 'ISFJ', name: 'مدافع', color: '#5fafec' },
+    { code: 'ESTJ', name: 'مدیر', color: '#5fafec' },
+    { code: 'ESFJ', name: 'سفیر', color: '#5fafec' },
 
-    'ISTP',
-    'ISFP',
-    'ESTP',
-    'ESFP'
-  ];;
+    { code: 'ISTP', name: 'ماهر', color: '#e6d433' },
+    { code: 'ISFP', name: 'ماجراجو', color: '#e6d433' },
+    { code: 'ESTP', name: 'کارآفرین', color: '#e6d433' },
+    { code: 'ESFP', name: 'سرگرم‌کننده', color: '#e6d433' }
+  ];
 
   months: string[] = [
     "فروردین",
