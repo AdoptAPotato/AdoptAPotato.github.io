78b7c12a98deb1ba5ec0be3fcea0a234b476b360
NegarMov <movaghatian.negar@gmail.com>
2026-08-06 19:16:20 +0330

    add filters to adoption page

--- src/app/pages/adoption/adoption.component.ts
+++ src/app/pages/adoption/adoption.component.ts
@@ -1,15 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { PotatoCardComponent } from '../../components/potato-card/potato-card.component';
+import { MBTI_TYPES, MONTHS } from '../../data/constants';
+import { Potato } from '../../models/potato';
 
 @Component({
   selector: 'app-adoption',
-  imports: [CommonModule, PotatoCardComponent],
+  imports: [CommonModule, FormsModule, PotatoCardComponent],
   templateUrl: './adoption.component.html',
   styleUrl: './adoption.component.css'
 })
 export class AdoptionComponent {
+
+  filteredPotatoes: Potato[] = [];
+  
+  filterMbti: string = '';
+  filterMonth: string = '';
+  
+  get mbtiTypes() {
+    return MBTI_TYPES;
+  }
+  
+  get months() {
+    return MONTHS;
+  }
+
+  ngOnInit() {
+    this.filteredPotatoes = [...this.potatoes];
+  }
+
+  applyFilters() {
+    this.filteredPotatoes = this.potatoes.filter(potato => {
+      const matchMbti = !this.filterMbti || potato.mbti === this.filterMbti;
+      const matchMonth = !this.filterMonth || potato.birthMonth === this.filterMonth;
+      return matchMbti && matchMonth;
+    });
+  }
+
+  resetFilters() {
+    this.filterMbti = '';
+    this.filterMonth = '';
+    this.applyFilters();
+  }
 
   potatoes = [
     {
